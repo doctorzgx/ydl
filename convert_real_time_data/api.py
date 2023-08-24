0171ddff43882875c9befe0affc2784150f45230
张高新 <1946620995@qq.com>
2023-08-24 17:03:45 +0800

    update

--- convert_real_time_data/api.py
+++ convert_real_time_data/api.py
@@ -5,8 +5,9 @@ import re
 import json
 import pandas as pd
 import pymysql
+import logging
 from datetime import datetime, timedelta
-from flask import Flask
+from flask import Flask, Response
 from sqlalchemy import create_engine
 
 sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding="utf8", line_buffering=True)
@@ -19,6 +20,7 @@ def generate_insert_sql_real_time_data(last_update_time):
     data = pd.read_sql('''SELECT * FROM db_ydl_dev.cm_ydl_history where updateTime >= '{}' '''
                        .format(last_update_time), engine)
     start_time, end_time = data['updateTime'].min(), data['updateTime'].max()
+    logging.info('start_time: {}  end_time: {}'.format(start_time, end_time))
     count = 0
     updateTime = []
     df_dict = dict()
@@ -26,7 +28,7 @@ def generate_insert_sql_real_time_data(last_update_time):
     just = datetime.now().timestamp()
     data.drop_duplicates(subset=['name', 'value', 'updateTime'], keep='first', inplace=True)
     data['name'] = data.apply(lambda x: re.sub(pattern, '_', x['name'] + 'type{}'.format(x['type'])), axis=1)
-    for _ in range((end_time - start_time).second):
+    for _ in range((end_time - start_time).seconds):
         start_time += timedelta(seconds=1)
         time_df = data[data['updateTime'] == start_time]
         if time_df.empty:
@@ -51,7 +53,7 @@ def generate_insert_sql_real_time_data(last_update_time):
             print(set([len(_) for _ in df_dict.values()]))
         count += 1
 
-        print('所用时间', datetime.now().timestamp() - just, 'count:', count)
+    logging.info('所用时间 {} count {}'.format(datetime.now().timestamp() - just, count))
 
     df = pd.DataFrame(df_dict)
     df.insert(0, 'updateTime', updateTime)
@@ -72,8 +74,8 @@ def insert_sql_real_time_data():
 
     select_query = "SELECT updateTime FROM cm_ydl_history_real_time ORDER BY updateTime DESC LIMIT 1"
     cursor.execute(select_query)
-    last_update_time = cursor.fetchone()
-    last_update_time = last_update_time[0] if last_update_time else None
+    last_update_time = cursor.fetchone()[0]
+    logging.info('last_update_time: {}'.format(last_update_time))
 
     cursor.execute("DELETE FROM cm_ydl_history_real_time WHERE updateTime = '{}';".format(last_update_time))
     conn.commit()
@@ -100,6 +102,7 @@ def insert_sql_real_time_data():
         conn.commit()
     # engine = create_engine('mysql+pymysql://bigdata:BBbb11335577!@192.168.3.104:3306/db_ydl_dev')
     data.to_sql(name='cm_ydl_history_real_time', con=engine, if_exists='append', index=False)
+    logging.info('convert finish')
     # 关闭游标和连接
     cursor.close()
     conn.close()
@@ -110,10 +113,14 @@ def insert_sql_real_time_data():
 def Convert_real_time_data():
     insert_sql_real_time_data()
 
+    return Response(status=200)
+
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='./convert_data.log', level=logging.INFO, encoding='utf-8')
     config = json.load(open('./config_file.json', encoding='utf-8'))
     url = 'mysql+pymysql://{}:{}@{}: {}/{}'.format(config['mysql_user'], config['mysql_password'],
                                                    config['mysql_host'], config['mysql_port'], config['mysql_database'])
     engine = create_engine(url)
-    app.run(host=config['API_http_host'], port=config['API_http_port'], debug=True)
+    # app.run(host=config['API_http_host'], port=config['API_http_port'], debug=True)
+    app.run(host=config['API_http_host'], port=config['API_http_port'])
